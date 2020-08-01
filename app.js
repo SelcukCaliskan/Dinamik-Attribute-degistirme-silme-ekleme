7d9c9c049d81c4dd7718cda87ec74cdf57d96a67
Selçuk Çalışkan <45595456+SelcukCaliskan@users.noreply.github.com>
2020-08-01 15:20:35 +0300

    Dinamik Attribute-değiştirme silme,ekleme
    Signed-off-by: Selçuk Çalışkan <45595456+SelcukCaliskan@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,23 @@
+const todoInput=document.getElementById("todo");
+let element;
+
+element=todoInput;
+element=todoInput.classList;
+
+// todoInput.classList.add("newClass1");
+// todoInput.classList.add("newClass2");
+
+// todoInput.classList.remove("form-control");
+
+
+element=todoInput;
+element=todoInput.placeholder;
+element=todoInput.getAttribute("placeholder");
+todoInput.setAttribute("placeholder","Naber");
+todoInput.setAttribute("title","New Title");
+element=todoInput;
+//element=todoInput.hasAttribute("name");
+todoInput.removeAttribute("name");
+
+element=todoInput;
+console.log(element);
